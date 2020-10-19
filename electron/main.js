7255870c0f2cf86418ff4e7099d823f7d02d1cab
Mahdi Nicoo <MahdiNicoo@gmail.com>
2020-10-19 15:43:49 +0330

    electron simple start

---
+++ electron/main.js
@@ -0,0 +1,43 @@
+
+const path = require('path')
+const url = require('url')
+const { app, BrowserWindow } = require('electron')
+
+let mainWindow
+
+function createWindow () {
+    console.log('process.env.ELECTRON_START_URL', process.env.ELECTRON_START_URL)
+    const startUrl = process.env.ELECTRON_START_URL || url.format({
+        pathname: path.join(__dirname, '../index.html'),
+        protocol: 'file:',
+        slashes: true,
+    })
+
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    win.loadURL(startUrl)
+    win.webContents.openDevTools()
+
+    mainWindow = win
+    mainWindow.on('closed', () => mainWindow = null)
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
